b7f07fd77f23846afb5711d57e34a8ee21323e8a
Daniel Torres <danieltorres31@outlook.com>
2019-03-27 20:35:55 -0300

    feat: configura rotas usuario e login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
+import { AuthGuard } from './guard/auth.guard';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { UsuarioNovoComponent } from './componentes/usuario/usuario-novo/usuario-novo.component';
+import { LoginComponent } from './componentes/login/login.component';
+import { UsuarioComponent } from './componentes/usuario/usuario.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'login', component: LoginComponent},
+  
+  //Rotas Usuario
+  {path: 'usuario/novo', component: UsuarioNovoComponent, canActivate: [AuthGuard]},
+  {path: 'usuario', component: UsuarioComponent, canActivate: [AuthGuard]},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
