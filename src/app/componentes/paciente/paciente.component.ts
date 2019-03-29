714f633cb6bef45a3acb57e5c9a7127997abda98
Carlos Costa <carlosvini94@gmail.com>
2019-03-29 14:49:34 -0300

    fix: corrige imports em paciente.component

--- src/app/componentes/paciente/paciente.component.ts
+++ src/app/componentes/paciente/paciente.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Paciente } from './shared/paciente.model';
 
 @Component({
   selector: 'app-paciente',
@@ -6,6 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./paciente.component.scss']
 })
 export class PacienteComponent implements OnInit {
+
+  titulo: string = 'Lista de Pacientes';
+
+  colunas: string[] = ['nome', 'email', 'sexo', 'cpf', 'data nascimento'];
+  pacientes: Paciente[] = [
+    {nome: 'Joelma', sexo: 'Feminino', email: 'joelma@email.com',  cpf: 10043718612, data_atendimento: '21/01/1980'},
+    {nome: 'Ximbinha', sexo: 'Masculino', email: 'ximbinha@email.com',  cpf: 10043718612, data_atendimento: '21/01/1994'},
+    {nome: 'Eduardo', email: 'eduardo@email.com', sexo: 'Masculino', cpf: 10043718612, data_atendimento: '21/01/1985'},
+    {nome: 'Henry', email: 'henry@email.com', sexo: 'Masculino', cpf: 10043718612, data_atendimento: '21/01/1982'},
+  ];
+
 
   constructor() { }
 
--- src/app/componentes/paciente/paciente.module.ts
+++ src/app/componentes/paciente/paciente.module.ts
@@ -2,10 +2,22 @@ import { PacienteComponent } from './paciente.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { CardModule } from 'primeng/card';
+import { ButtonModule } from 'primeng/button';
+import { FormsModule } from '@angular/forms';
+import { GtmTabelaModule } from '../gtm-tabela/gtm-tabela.module';
+
+
+
+
 @NgModule({
   declarations: [PacienteComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    CardModule,
+    ButtonModule,
+    FormsModule,
+    GtmTabelaModule
   ]
 })
 export class PacienteModule { }
