1cb2e195ee8e7214bf5f388d8ac3c04d6929f2d4
Daniel Torres <danieltorres31@outlook.com>
2019-03-29 20:26:42 -0300

    feat: Cria estrutura básica atendimento

---
+++ src/app/componentes/atendimento/atendimento.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AtendimentoComponent } from './atendimento.component';
+
+@NgModule({
+  declarations: [AtendimentoComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class AtendimentoModule { }
