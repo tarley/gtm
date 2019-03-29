be94aac1f4fc925dc23732198771e4c9474ea18a
Carlos Costa <carlosvini94@gmail.com>
2019-03-29 14:35:59 -0300

    feat: Inclui feature de listagem de pacientes

---
+++ src/app/componentes/paciente/shared/paciente.model.ts
@@ -0,0 +1,10 @@
+export class Paciente {
+
+    public constructor(
+        public nome: string,
+        public sexo: string,
+        public cpf: number,
+        public data_atendimento: string,
+        public email: string,
+    ) {}
+}
