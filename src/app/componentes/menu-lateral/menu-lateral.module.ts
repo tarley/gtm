1b6b037c4ff2a1a05c6e431cd87414e1e10760fa
Daniel Torres <danieltorres31@outlook.com>
2019-04-04 23:52:38 -0300

    feat: Implementa componente menu lateral

---
+++ src/app/componentes/menu-lateral/menu-lateral.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MenuLateralComponent } from './menu-lateral.component';
+import { MenuModule } from 'primeng/menu';
+import { SidebarModule } from 'primeng/sidebar';
+
+
+@NgModule({
+  declarations: [MenuLateralComponent],
+  imports: [
+    CommonModule,
+    MenuModule,
+    SidebarModule,
+  ], 
+  exports: [MenuLateralComponent]
+})
+export class MenuLateralModule { }
