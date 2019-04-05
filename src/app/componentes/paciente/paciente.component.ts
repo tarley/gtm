69131da1bf298f292c0f5066fe5f773d1125f6b6
Carlos Costa <carlosvini94@gmail.com>
2019-04-05 15:50:07 -0300

    fix: corrige listagem de paciente

--- src/app/componentes/paciente/paciente.component.ts
+++ src/app/componentes/paciente/paciente.component.ts
@@ -19,6 +19,7 @@ export class PacienteComponent implements OnInit {
   constructor(private router: Router, private pacienteService: PacienteService) { }
 
   ngOnInit() {
+    this.buscarTodos()
   }
 
   navigate(route: string) {
