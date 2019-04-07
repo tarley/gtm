3b9f9af1dc51abb34e12bf9bd5966a25a492b02d
Daniel Torres <danieltorres31@outlook.com>
2019-04-07 11:02:35 -0300

    feat: Ajusta paginacao e cria emissor de eventos ao selecionar linha

--- src/app/componentes/gtm-tabela/gtm-tabela.module.ts
+++ src/app/componentes/gtm-tabela/gtm-tabela.module.ts
@@ -4,7 +4,6 @@ import { GtmTabelaComponent } from './gtm-tabela.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ButtonModule } from 'primeng/button';
 import { TableModule } from 'primeng/table';
-import { PaginatorModule } from 'primeng/paginator';
 import { GtmTabelaService } from './gtm-tabela.service';
 import { ConfirmDialogModule } from 'primeng/confirmdialog';
 import { ConfirmationService } from 'primeng/api';
@@ -16,7 +15,6 @@ import { ConfirmationService } from 'primeng/api';
     BrowserAnimationsModule,
     ButtonModule,
     TableModule,
-    PaginatorModule,
     ConfirmDialogModule,
   ],
   exports: [GtmTabelaComponent],
