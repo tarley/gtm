aa86c5be808a82f8e198292caf092c87452d47aa
Daniel Torres <danieltorres31@outlook.com>
2019-04-15 19:54:39 -0300

    feat: Adiciona constantes SCF e PRM

--- src/app/util/constantes.ts
+++ src/app/util/constantes.ts
@@ -32,4 +32,83 @@ export class Constantes {
         { label: 'Encaminhamento pela pela UBS,', value: 'clincaUbs' },
         { label: 'Outro Encaminhamento,', value: 'outroEncaminhamento' },
     ]
+
+    public static scf = [
+        { label: 'Estável', value: 'Estável' },
+        { label: 'Falha', value: 'Falha' },
+        { label: 'Inicial', value: 'Inicial' },
+        { label: 'Melhora', value: 'Melhora' },
+        { label: 'Melhora Parcial', value: 'Melhora Parcial' },
+        { label: 'Morte', value: 'Morte' },
+        { label: 'Piora', value: 'Piora' },
+        { label: 'Resolvido', value: 'Resolvido' },
+        { label: 'Sem Melhora', value: 'Sem Melhora' },
+        { label: 'Sem Condições de Avaliar', value: 'Sem Condições de Avaliar' },
+    ]
+
+    public static prms = [
+        {
+            label: 'PRM 1 - Medicamento desnecessário', value: 'PRM1', causas: [
+                { label: 'Ausência de indicação clínica no momento', value: 'Ausência de indicação clínica no momento' },
+                { label: 'Uso de múltiplos med quando apenas um (ns) resolveria (m)', value: 'Uso de múltiplos med quando apenas um (ns) resolveria (m)' },
+                { label: 'Medicamento não é efetivo para a condição', value: 'Medicamento não é efetivo para a condição' },
+                { label: 'Terapia não medicamentosa mais apropriada', value: 'Terapia não medicamentosa mais apropriada' },
+                { label: 'Tratamento de reação que poderia ter sido prevenida', value: 'Tratamento de reação que poderia ter sido prevenida' },
+                { label: 'Uso recreacional', value: 'Uso recreacional' },
+            ]
+        },
+        {
+            label: 'PRM 2 - Necessidade de medicamento', value: 'PRM2', causas: [
+                { label: 'Presença de uma condição clínica que requer o uso de meds', value: 'Presença de uma condição clínica que requer o uso de meds' },
+                { label: 'Tratamento profilático necessário para reduzir risco de outro problema', value: 'Tratamento profilático necessário para reduzir risco de outro problema' },
+                { label: 'Tratamento adicional/sinérgico necessário para obter efeito desejado', value: 'Tratamento adicional/sinérgico necessário para obter efeito desejado' },
+            ]
+        },
+        {
+            label: 'PRM 3 - Medicamento não é efetivo para a condição', value: 'PRM3', causas: [
+                { label: 'O medicamento usado não é o mais efetivo para a condição tratada', value: 'O medicamento usado não é o mais efetivo para a condição tratada' },
+                { label: 'A condição tratada é refratária ao medicamento usado', value: 'A condição tratada é refratária ao medicamento usado' },
+                { label: 'O medicamento não efetivo para o transtorno', value: 'O medicamento não efetivo para o transtorno' },
+                { label: 'Presença de contra indicação', value: 'Presença de contra indicação' },
+            ]
+        },
+        {
+            label: 'PRM 4 – Dose', value: 'PRM4', causas: [
+                { label: 'A dose é muito baixa para  produzir a resposta desejada', value: 'A dose é muito baixa para  produzir a resposta desejada' },
+                { label: 'Intervalo entre doses maior que o necessário para se alcançar objetivos', value: 'Intervalo entre doses maior que o necessário para se alcançar objetivos' },
+                { label: 'Uma interação reduz a quantidade disponível do fármaco', value: 'Uma interação reduz a quantidade disponível do fármaco' },
+                { label: 'Duração do trat. menor que necessário para se obter o efeito desejado', value: 'Duração do trat. menor que necessário para se obter o efeito desejado' },
+                { label: 'Administração incorreta', value: 'Administração incorreta' },
+                { label: 'Armazenamento incorreto', value: 'Armazenamento incorreto' },
+            ]
+        },
+        {
+            label: 'PRM 5 - Reação adversa ao medicamento', value: 'PRM5', causas: [
+                { label: 'O medicamento produz efeito indesejável que não é relacionado com a dose', value: 'O medicamento produz efeito indesejável que não é relacionado com a dose' },
+                { label: 'O medicamento produz uma reação alérgica', value: 'O medicamento produz uma reação alérgica' },
+                { label: 'O medicamento não é seguro (presença de fatores risco/contra indicação)', value: 'O medicamento não é seguro (presença de fatores risco/contra indicação)' },
+                { label: 'Interação causa uma reação que não é dose relacionada', value: 'Interação causa uma reação que não é dose relacionada' },
+                { label: 'Administração incorreta (dose administrada ou alterada muito rapidamente)', value: 'Administração incorreta (dose administrada ou alterada muito rapidamente)' },
+            ]
+        },
+        {
+            label: 'PRM 6 - Dose alta', value: 'PRM6', causas: [
+                { label: 'A dose é muito alta', value: 'A dose é muito alta' },
+                { label: 'O intervalo entre as doses é menor que o recomendado', value: 'O intervalo entre as doses é menor que o recomendado' },
+                { label: 'A duração do tratamento é maior que o necessário', value: 'A duração do tratamento é maior que o necessário' },
+                { label: 'A interação causa uma reação dose relacionada', value: 'A interação causa uma reação dose relacionada' },
+                { label: 'A dose do med foi administrada muito rapidamente', value: 'A dose do med foi administrada muito rapidamente' },
+            ]
+        },
+        {
+            label: 'PRM 7 - Não adesão', value: 'PRM7', causas: [
+                { label: 'O paciente não compreendeu as instruções', value: 'O paciente não compreendeu as instruções' },
+                { label: 'O paciente prefere não utilizar o medicamento', value: 'O paciente prefere não utilizar o medicamento' },
+                { label: 'O paciente esquece de utilizar o medicamento', value: 'O paciente esquece de utilizar o medicamento' },
+                { label: 'O medicamento é muito caro para o paciente', value: 'O medicamento é muito caro para o paciente' },
+                { label: 'O paciente não consegue engolir/administrar o med adequadamente', value: 'O paciente não consegue engolir/administrar o med adequadamente' },
+                { label: 'O produto não está disponível para o paciente', value: 'O produto não está disponível para o paciente' },
+            ]
+        },
+    ]
 }
