47ba7fc5c0f53f52d4c5f09d22fa3eb53e0619ad
Henrique Gomes <henriquecebrac37@gmail.com>
2019-06-22 16:02:31 -0300

    Fix: Ajusta listagem usuario

--- src/app/componentes/usuario/usuario.component.ts
+++ src/app/componentes/usuario/usuario.component.ts
@@ -5,6 +5,8 @@ import { Usuario } from './shared/usuario.model';
 import { Router } from '@angular/router';
 import { MessageServiceUtil } from 'src/app/util/message-service-util.service';
 import { BlockUI, NgBlockUI } from 'ng-block-ui';
+import { InstituicaoService } from '../instituicao/shared/instituicao.service';
+import { AuthService } from '../login/shared/auth.service';
 
 @Component({
   selector: 'app-usuario',
@@ -28,15 +30,22 @@ export class UsuarioComponent implements OnInit {
   ];
   usuarios: Usuario[] = [];
 
-  constructor(private router: Router, private usuarioService: UsuarioService, private messageService: MessageServiceUtil) { }
+  constructor(private router: Router, private usuarioService: UsuarioService, private messageService: MessageServiceUtil,
+              private instituicaoService: InstituicaoService, private auth: AuthService) { }
 
   ngOnInit() {
     this.buscarTodos();
   }
 
   buscarTodos() {
+
     this.blockUI.start(MensagemUtil.CARREGANDO_REGISTRO);
     this.usuarioService.buscarTodos().subscribe((usuarios: Usuario[]) => {
+      usuarios.forEach(usuario =>{
+        this.instituicaoService.buscarPorId(usuario.instituicao).subscribe((instituicao: any) => {
+            usuario.instituicao = instituicao.descricao;
+        });
+      })
       this.usuarios = usuarios;
     }, () => {
           this.messageService.add(MensagemUtil.criaMensagemErro(MensagemUtil.ERRO_BUSCAR));
